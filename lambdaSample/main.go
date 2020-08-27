7bf8c03054a8d77ceb6f2d8fc45fce31d8ecd693
umanari145 <umanari145@gmail.com>
2020-08-27 22:36:58 +0900

    郵便番号を受け付けるzpiの作成

--- lambdaSample/main.go
+++ lambdaSample/main.go
@@ -1,16 +1,24 @@
 package main
 
 import (
+	"context"
 	"fmt"
-	"os"
 
+	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler() {
+func loadAreaFromZip(zipCode string) {
+	fmt.Println("------")
+	fmt.Println(zipCode)
+	fmt.Println("------")
+}
 
-	fmt.Println(os.Getenv("TEST"))
-	os.Exit(0)
+//urlのパターンは　/area/274-0077(郵便番号)
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (string, error) {
+	zipCode := request.PathParameters["zipCode"]
+	loadAreaFromZip(zipCode)
+	return "", nil
 }
 
 func main() {
